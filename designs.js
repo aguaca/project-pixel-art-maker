c5030c93118ef2b141c8d6b11f20a704e3ae1aaf
Isaias Geronimo <mprii@hotmail.com>
2022-04-08 18:04:58 -0400

    Set the size of the cross stitch canvas

--- designs.js
+++ designs.js
@@ -2,10 +2,23 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-  const form = document.getElementById('sizePicker');
+const form = document.getElementById('sizePicker');
+function makeGrid(event) {
+  event.preventDefault();
   const height = form.height.value;
   const width = form.width.value;
   const table = document.getElementById('pixelCanvas');
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
+  }
+
+  for (let h = 0; h < height; h += 1) {
+    const tr = document.createElement('tr');
+    for (let w = 0; w < width; w += 1) {
+      const td = document.createElement('td');
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
 }
+form.addEventListener('submit', makeGrid);
