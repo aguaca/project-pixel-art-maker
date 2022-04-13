b9ce1673d7488ec895bd1d0333fea743589d55a4
Isaias Geronimo <mprii@hotmail.com>
2022-04-13 17:34:08 -0400

    added additional comments
    also  used Code Beautify

--- designs.js
+++ designs.js
@@ -1,34 +1,44 @@
 // Select color input
 // Select size input
-
 // When size is submitted by the user, call makeGrid()
+
 // table shift before color pick
-const form = document.getElementById('sizePicker');
 const color = document.getElementById('colorPicker');
-color.addEventListener('focusin', () => { color.style.marginBottom = '230px'; });
-color.addEventListener('focusout', () => { color.style.marginBottom = '0px'; });
+color.addEventListener('focusin', () => {
+	color.style.marginBottom = '230px';
+});
+color.addEventListener('focusout', () => {
+	color.style.marginBottom = '0px';
+});
+
+//getting form element
+const form = document.getElementById('sizePicker');
 function makeGrid(event) {
-  event.preventDefault();
-  const height = form.height.value;
-  const width = form.width.value;
+	event.preventDefault();
+	//saving input to variables
+	const height = form.height.value;
+	const width = form.width.value;
+	//getting table element
+	const table = document.getElementById('pixelCanvas');
+	//clear table everytime makeGrid gets call
+	while (table.firstChild) {
+		table.removeChild(table.firstChild);
+	}
+	//creating table based on width and heigth
+	for (let h = 0; h < height; h += 1) {
+		const tr = document.createElement('tr');
+		for (let w = 0; w < width; w += 1) {
+			const td = document.createElement('td');
+			const assignColor = () => {
+				td.style.backgroundColor = color.value;
+			};
+			//adding click event listener for every cell in table
+			td.addEventListener('click', assignColor);
 
-  const table = document.getElementById('pixelCanvas');
-  while (table.firstChild) {
-    table.removeChild(table.firstChild);
-  }
-
-  for (let h = 0; h < height; h += 1) {
-    const tr = document.createElement('tr');
-    for (let w = 0; w < width; w += 1) {
-      const td = document.createElement('td');
-      const assignColor = () => {
-        td.style.backgroundColor = color.value;
-      };
-      td.addEventListener('click', assignColor);
-
-      tr.appendChild(td);
-    }
-    table.appendChild(tr);
-  }
+			tr.appendChild(td);
+		}
+		table.appendChild(tr);
+	}
 }
+//crating an event listener for the form
 form.addEventListener('submit', makeGrid);
